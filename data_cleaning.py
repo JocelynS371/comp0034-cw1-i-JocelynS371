2420cae7917d3215b1abfb213a2c7c064ca80bbe
Jocelyn <j.shao@hotmail.com>
2023-02-10 16:56:10 +0000

    Wrote some functions to clean data

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,9 +1,27 @@
-import matplotlib.pyplot as plt
+
 import pandas as pd
 
+def read_df():
+    #need to change column names
+    # data_path= Path(__file__).parent.joinpath('data', 'data_set_prepared.csv')
+    data_path='data_set_prepared.csv'
+    # cols = ['Temperture', 'Salinity', 'density', 'Pressure', 'Date', 'Longitude', 'Latitude', 'Bottom Depth']
+    df = pd.read_csv(data_path)
+    df.rename(columns={
+    'Potential_temperature_C':'Temperture',
+    'Practical_salinity':'Salinity',
+    'Potential_density_anomaly_kgm3':'Density',
+    'Pressure_decibar':'Pressure',
+    'Serial_date_number_base_date_1_January_0000':'Date',
+    'Bottom_Depth_m':'Bottom Depth'
+    },inplace=True)
+    return df
+def location(df):
+    grouped = df.groupby('Longitude','Latitude')
 
 def main():
-    df=pd.read_csv(data_set_prepared.csv)
-    column_label=df.columns
-    print(df.shape)
+    df=read_df()
+    column_labels=df.columns
+    print(column_labels)
+
 main()
