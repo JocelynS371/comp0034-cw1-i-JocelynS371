289f8bf0ef62901f945e889f4bbbaba0d0d5f7b2
Jocelyn <j.shao@hotmail.com>
2023-02-12 21:20:02 +0000

    added callback

--- app.py
+++ app.py
@@ -23,13 +23,26 @@ def read_df():
     'Serial_date_number_base_date_1_January_0000':'Date',
     'Bottom_Depth_m':'Bottom Depth'
     },inplace=True)
+    df['Latitude'] = round(df['Latitude'], 2)
+    df['Longitude'] = round(df['Longitude'], 2)
     return df
-df=read_df()
-def seperate_location(df):
-    
-    return df
-df=read_df()
+def temp_plot(df):
+    figures = {}
+    for i, (name, group) in enumerate(grouped):
+        fig = px.scatter(
+            group,
+            x='Date',
+            y='Temperture',
+            template='simple_white')
+        title = f"Longitude: {name[0]} Latitude: {name[1]}"
+        fig.update_layout(title=title)
+        figures[f"fig_{i + 1}"] = fig
+    return figures
 
+df=read_df()
+grouped = df.groupby(['Longitude', 'Latitude'])
+temp_plot_fig=temp_plot(df)
+print(len(temp_plot_fig))
 line_temp=px.line(
     df,
     x='Date',
@@ -43,19 +56,27 @@ app.layout=dbc.Container(children=[
             dbc.Col(width=3, children=[
                 html.H4('select location'),
                 dcc.Dropdown(
-                    id='location-select',
-                    options=['A','B'],
-                    value='A'
+                    id='figure-dropdown',
+                    options=[{'label': f'Location {i+1}', 'value': f'fig_{i+1}'} 
+                    for i in range(len(temp_plot_fig))],
+                    value='fig_1'
                 )]),
             dbc.Col(width=9, children=[
-                html.H2(children='graph of temperture change with time'),
+                html.H2(children='Graph of temperture change with time'),
                 dcc.Graph(
                 id='temp_line',
-                figure=line_temp)]),
+                figure=temp_plot_fig['fig_1'])]),
         ])
             
     ]
 )
+@app.callback(
+    Output(component_id='temp_line', component_property='figure'),
+    [Input(component_id='figure-dropdown', component_property='value')]
+)
+def update_figure(selected_value):
+    figures = temp_plot(df)
+    return figures[selected_value]
 
 if __name__=='__main__':
     app.run_server(debug=True)
